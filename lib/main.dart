c2ed491a76e90602abc375a7a6260fa8f58a90c0
ahmed-elmaghwry <ahmed.hassan.elmaghwry@gmail.com>
2025-01-07 19:56:45 +0200

    configure supabase with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'core/helper_functions/on_generate_routes.dart';
 import 'core/services/custom_bloc_observer.dart';
 import 'core/services/get_it_service.dart';
@@ -8,8 +9,14 @@ import 'features/dashboard/views/dashboard_view.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  print('main');
+  await Supabase.initialize(
+    url: 'https://ezdgwtvgtcwuidfnhdlf.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6ZGd3dHZndGN3dWlkZm5oZGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyMTU1NTQsImV4cCI6MjA1MTc5MTU1NH0.g6plC_Cmuxn5sSCTNQRRPPN5ZbFqHYtX30W4L_lCLFE',
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
+
 
   Bloc.observer = CustomBlocObserver();
   // Initialize Firebase with error handling
@@ -30,6 +37,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: DashboardView.routeName,
       onGenerateRoute: onGenerateRoute,
     );
